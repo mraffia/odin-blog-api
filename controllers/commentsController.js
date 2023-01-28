8d33bddeff5f07b1d36390ea62e8ebf223f62525
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-29 01:56:57 +0700

    Implement create a comment controller

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,6 +1,58 @@
 const Comment = require("../models/comment");
 
+const async = require('async');
+const { body, validationResult } = require("express-validator");
+
 // Handle Comment create
-exports.comments_create = (req, res) => {
-  res.send("NOT IMPLEMENTED: Comment create POST");
-};
+exports.comments_create = [
+  // Validate and sanitize fields.
+  body("post", "Post must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("author", "Author must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("comment_content", "Content must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("comment_timestamp", "Invalid date")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Return data with sanitized values/errors messages.
+      res.json({
+        comment: req.body,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // Data from form is valid.
+    // Create a Comment object with escaped and trimmed data.
+    const comment = new Comment({
+      post: req.body.post,
+      author: req.body.author,
+      content: req.body.comment_content,
+      timestamp: req.body.comment_timestamp,
+    });
+
+    comment.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.json({
+        comment: comment,
+        comment_url: comment.url
+      });
+    });
+  },
+];
--- routes/comments.js
+++ routes/comments.js
@@ -4,6 +4,6 @@ var router = express.Router();
 const comments_controller = require("../controllers/commentsController");
 
 // POST request for creating Comment.
-router.post("/comments", comments_controller.comments_create);
+router.post("/", comments_controller.comments_create);
 
 module.exports = router;
