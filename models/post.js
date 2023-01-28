a3c552f4be71fd864346c019758b22afd66a96d7
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-28 20:53:08 +0700

    Create Post model

---
+++ models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  timestamp: { type: Date, required: true, default: Date.now },
+  is_published: { type: Boolean, required: true, default: false },
+});
+
+// Virtual for post's URL
+PostSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/post/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Post", PostSchema);
