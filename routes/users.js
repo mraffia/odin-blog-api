db9ec2ff14d279f79d17772da40f1eb2ba484cad
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-29 20:59:09 +0700

    Remove users routes and controllers

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,4 @@
 var express = require('express');
 var router = express.Router();
 
-const users_controller = require("../controllers/usersController");
-
-// POST request for creating User.
-router.post("/", users_controller.users_create);
-
 module.exports = router;
