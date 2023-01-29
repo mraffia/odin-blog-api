632af91a557dd80cc003bfde6b5e421a9f62afc1
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-29 21:27:54 +0700

    Add second argument to mongoose.connect

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const mongoDB = process.env.MONGODB_URI;
 
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect(mongoDB);
+  await mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
 app.use(logger('dev'));
